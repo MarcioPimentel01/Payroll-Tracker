97b70ff98aa958457681cb6095f4f9c14c91b375
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-03-22 00:20:32 -0400

    fixed employee push int the array, firstName, lastName, and salary had to be assigned

--- Develop/script.js
+++ Develop/script.js
@@ -6,20 +6,27 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
-  const employee = [];  //creation of an array to store employee names
+  const employees = [];  //creation of an array to store employee names
   let adding = true; //this variable has the intent to control the loop
   while (adding) {
-    let firstName = prompt("PLease enter employee's First Name");
-    let lastName = prompt("Please enter employee's last name.");
-    let salary = parseFloat (prompt("Please, input the salary amount.")); //here I'm using parsiFloat function that trnasforms the string argument into a number
+      let firstName = prompt("Please enter employee's First Name");
+      let lastName = prompt("Please enter employee's last name.");
+      let salary = parseFloat (prompt("Please, input the salary amount.")); //here I'm using parseFloat function that transforms the string argument into a number
+      
+      // Conditional to check if the salary is a valid number, and zero by default in case there is no input.
+      if (isNaN(Number(salary)) || salary < 0) { 
+          alert("Invalid salary amount. Please enter a valid positive number.");
+      } else {
+          const employee = { // Creating an object to represent an employee
+              firstName: firstName, // Using the lastName variable
+              lastName: lastName, // Using the lastName variable
+              salary: salary // Using the lastName variable
+          };
+          employees.push(employee); // Pushing the employee object to the employees array
+      }
   }
 
-// conditiona to check if the salary is a valid number, and zero be default in case there is no imnput.
-if (isNaN(Number(salary) || salary < 0)) { 
-  alert("Invalid salary amount. Please enter a valid positive number.")
-} else {
-  employee.push({firstName: firstName, lastName: lastName, salary: salary}) //use of push to insert data into the array 'employee'.
-} adding = confirm("Do you want to add another employee?"); //using the variable a"dding = true" to pop up a window with "confirm" with ok and cancel
+  return employees; // Returning the array of collected employees
 }
 
 // Display the average salary
@@ -30,6 +37,7 @@ const displayAverageSalary = function(employeesArray) {
     salaryTotal = salaryTotal + employee.salary;
   }
   const averageSalary = salaryTotal / employeesArray.length; 
+  console.log(`Average Salary: $${averageSalary.toFixed(2)}`); // Display the average salary in the console
 }
 
 // Select a random employee
